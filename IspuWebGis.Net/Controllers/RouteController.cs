4f2444eab194c9957f64481c0de98064de727cb1
Dreamer7771 <anatoly1410@gmail.com>
2017-10-30 10:34:52 +0300

    Added controllers
    Added dummy controllers, models

---
+++ IspuWebGis.Net/Controllers/RouteController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using IspuWebGis.Models;
+
+namespace IspuWebGis.Controllers
+{
+    [Route("api/route")]
+    public class RouteController : ApiController
+    {
+        [HttpPost]
+        public RouteModel Get([FromBody]RouteModel route)
+        {
+            int i = 0;
+            
+            foreach (var p in route.points)
+            {
+                p.id = i++;
+            }
+
+
+            return route;
+        }
+        public class RouteModel
+        { 
+            public List<CustomPoint> points { get; set; }
+        }
+    }
+}
